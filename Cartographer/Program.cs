e1da6cb4bffab3bbcce19c108cf229c973279331
Mike <msmith94@protonmail.com>
2019-04-01 18:42:57 +0100

    Add project files.

---
+++ Cartographer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cartographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
